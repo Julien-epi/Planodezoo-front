8768672eb4ab680488f3290b1082c43316e8affc
Julien-epi <julienranson77@gmail.com>
2023-07-17 12:26:36 +0200

    change url http

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,2 +1,2 @@
-// export const API_URL = 'http://16.170.253.137:3001/api/';
-export const API_URL = 'https://home.digitalwebworks.fr:3001/api/';
+export const API_URL = 'http://16.170.253.137:3001/api/';
+// export const API_URL = 'https://home.digitalwebworks.fr:3001/api/';
